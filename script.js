0e6a7c3e715b9884823b639f4503e87dd16bb1cb
Jamesoc23 <james.oc23@hotmail.com>
2019-08-24 22:17:05 +0000

    Added completed flag

--- script.js
+++ script.js
@@ -1,17 +1,27 @@
 
 var toDoList = {
-    todos: ["item 1", "item 2", "item 3"],
+    todos: [],
     displayToDos: function() {
       console.log("ToDos: ", this.todos)
     },
     addToDos: function(newToDo) {
-        return this.todos.push(newToDo);
+        this.todos.push({
+            toDoText: newToDo,
+            completed: false
+        });
     },
-    changeToDos: function(position, newToDo) {
-        return this.todos[i] = newToDo;
+    changeToDos: function(position, changeToDo) {
+        this.todos[i] = {
+            toDoText: changeToDo,
+            completed: false
+        };
     },
     removeToDos: function(position) {
-        return this.todos.splice(position, 1);
+        this.todos.splice(position, 1);
+    },
+    toggleToDos: function(position) {
+        var todo = this.todos[position];
+        todo.completed = !todo.completed;
     }
   }
 
