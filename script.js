03c770850a809942c060ddd10f073928a5caf431
Jamesoc23 <james.oc23@hotmail.com>
2019-08-24 22:37:10 +0000

    Added check to see if to do is empty

--- script.js
+++ script.js
@@ -3,6 +3,14 @@ var toDoList = {
     todos: [],
     displayToDos: function() {
       console.log("ToDos: ", this.todos)
+      if (this.todos.length === 0) {
+          console.log("Your to do list is empty")
+      } else {
+        for (i=0; i < this.todos.length; i++){
+          console.log(this.todos[i].toDoText);
+        }
+      }
+
     },
     addToDos: function(newToDo) {
         this.todos.push({
@@ -22,6 +30,7 @@ var toDoList = {
     toggleToDos: function(position) {
         var todo = this.todos[position];
         todo.completed = !todo.completed;
+        this.displayToDos();
     }
   }
 
